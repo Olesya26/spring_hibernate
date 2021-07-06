7415f75206e57611fbd98b1239878b88cbfcf3be
Olesya26 <metisochka.91@mail.ru>
2021-07-06 19:03:47 +0700

    решено

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,63 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
+
+        User user5 = new User("User5", "Lastname5", "user5@mail.ru");
+        User user6 = new User("User6", "Lastname6", "user6@mail.ru");
+        User user7 = new User("User7", "Lastname7", "user7@mail.ru");
+
+        Car car1 = new Car("Honda", 2586);
+        Car car2 = new Car("Audi", 8556);
+        Car car3 = new Car("Mercedes", 9654);
+
+        user5.setCar(car1);
+        user6.setCar(car2);
+        user7.setCar(car3);
+
+        userService.add(user5);
+        userService.add(user6);
+        userService.add(user7);
+
+        List<User> userList = userService.listUsers();
+        for (User user : userList) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+            System.out.println();
+        }
+
+        System.out.println(userService.getUserByCar("Audi", 8556));
+
+        context.close();
+    }
+}
---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,41 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCar(String model, int series) {
+        return sessionFactory
+                .getCurrentSession()
+                .createQuery("select user from User user inner join user.car c where " +
+                        "c.model= :model and c.series = :series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series)
+                .uniqueResult();
+
+    }
+}
